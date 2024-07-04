bb22594c3d2f52c62cfc7eeb5c17316c9f6176e8
SaschaBorowski <saschaborowski@outlook.de>
2024-07-04 10:10:19 +0200

    Added Edit function

--- JS/board/html_templates.js
+++ JS/board/html_templates.js
@@ -16,7 +16,7 @@ function userStoryHtmlTemplate() {
                         Kochwelt Page & Recipe Recommender
                     </h1>
                 </div>
-                <div class="taskDescription">
+                <div class="openUserStoryTaskDescription">
                     Build start page with recipe recommendation.
                 </div>
                 <div class="dueDateDateContainer">
@@ -54,7 +54,6 @@ function userStoryHtmlTemplate() {
                 <div class="userStorySubtaskContainer">
                     Subtasks
                 </div>
-
                 <div class="subtaskCheckboxHoverEffect">
                     <label class="container">
                         <input type="checkbox" checked="checked">
@@ -64,7 +63,6 @@ function userStoryHtmlTemplate() {
                         <p>Implement Recipe Recommendation</p>
                     </div>
                 </div>
-
                 <div class="subtaskCheckboxHoverEffect">
                     <label class="container">
                         <input type="checkbox" checked="checked">
@@ -76,16 +74,122 @@ function userStoryHtmlTemplate() {
                 </div>
                 <div class="userStoryDeleteAndEditContainer">
                     <div class="userStoryDeleteContainer userStoryBackgroundImageDelete">
-
                         <div class="userStoryDeleteTextContainer">Delete</div>
                     </div>
                     <div class="userStoryCutLine"></div>
-                    <div class="userStoryEditContainer userStoryBackgroundImageEdit">
-                        <div class="userStoryEditTextContainer">Edit</div>
+                    <div class="userStoryEditTextContainer userStoryBackgroundImageEdit">
+                        <div onclick="openUserStoryEdit()" class="userStoryEditTextTextContainer">Edit</div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    `
+}
+
+function userStoryEditHtmlTemplate() {
+    return `
+    <div class="userStoryBodyContainer">
+    <div class="userStoryEditContainer">
+        <div class="userStoryEditContainerInside">
+            <div class="userStoryEditCloseButtonContainer">
+                <img onclick="closeUserStoryEdit()" src="./img/userStoryEdit/close.png" alt="Close">
+            </div>
+            <form action="">
+                <div>
+                    <div class="mTop16">
+                        <label class="editTitleHeadlineContainer" for="title">Title</label>
+                    </div>
+                    <div class="editTitleInputContainer">
+                        <input type="text" required value="Kochwelt Page & Recipe Recommender">
+                    </div>
+                </div>
+                <div>
+                    <div class="mTop16">
+                        <label class="editDescriptionContainer" for="description">Description</label>
+                    </div>
+                    <div class="editDescriptionTextAreaContainer mTop8">
+                        <textarea name="description"
+                            id="description">Build start page with recipe recommendation</textarea>
+                    </div>
+                </div>
+                <div>
+                    <div class="mTop16">
+                        <label class="editDueDateContainer" for="Due date">Due date</label>
+                    </div>
+                    <div class="editDueDateInputContainer mTop8">
+                        <input type="date" required>
+                    </div>
+                </div>
+                <div>
+                    <div class="mTop16">
+                        <label class="editPriorityHeadline" for="priority">Priority</label>
+                    </div>
+                    <div class="editPriorityAllButtonsContainer">
+                        <div onclick="addUrgent()" id="urgent" class="editPriorityButtonContainer">
+                            <div>Urgent</div>
+                            <div>
+                                <img id="activeUrg" src="./img/userStoryEdit/urgent-prio-icon-inactive.svg" alt="High priority">
+                            </div>
+                        </div>
+                        <div onclick="addMedium()" id="medium" class="editPriorityButtonContainer">
+                            <div>Medium</div>
+                            <div>
+                                <img id="activeMed" src="./img/userStoryEdit/prio_medium_inactive.svg" alt="Medium priority">
+                            </div>
+                        </div>
+                        <div onclick="addLow()" id="low" class="editPriorityButtonContainer">
+                            <div>Low</div>
+                            <div>
+                                <img id="activeLow" src="./img/userStoryEdit/low-prio-icon-inactive.png" alt="Low priority">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mTop24">
+                    <label class="userStory_assignedToHeadline" for="assignedTo">Assigned to</label>
+                </div>
+                <div class="userStoryAssignedToInputAndImageContainer mTop8">
+                    <input type="text" placeholder="Select contacts to assign">
+                    <div class="userStoryAssignedToDropdownMenuImageContainer">
+                        <img src="./img/userStoryEdit/drop-down-arrow.png" alt="dropdownmenu">
+                    </div>
+                </div>
+                <div class="userStoryContactsContainer mTop8">
+                    <div class="userStoryContacts">as</div>
+                    <div class="userStoryContacts">de</div>
+                    <div class="userStoryContacts">ef</div>
+                </div>
+                <div class="userStorySubtaskHeadlineContainer mTop16">
+                    <label class="userStorySubtaskHeadline" for="subtasks">Subtasks</label>
+                </div>
+                <div class="userStorySubtasksInputContainer mTop8">
+                    <input type="text" placeholder="Add new subtask">
+                    <div class="userStorySubtasksDropdownImageContainer">
+                        <img src="./img/userStoryEdit/add-plus-icon.png" alt="dropdownmenu">
+                    </div>
+                </div>
+                <div class="userStorySubtasksContainer mTop8">
+                    <table>
+                        <div class="userStorySubtaskListedItemContainer subtaskListedItem0">
+                            <li>Implement Recipe Recommendation</li>
+                            <div class="userStorySubtaskListedItemImageContainer subtaskListedImage0">
+                                <img class="mRight8" src="./img/userStoryEdit/edit-dark.png" alt="edit">
+                                <img class="mRight16" src="./img/userStoryEdit/delete.png" alt="delete">
+                            </div>
+                        </div>
+                        <div class="userStorySubtaskListedItemContainer subtaskListedItem1">
+                            <li class="userStorySubtaskListedItem">Start Page Layout</li>
+                            <div class="userStorySubtaskListedItemImageContainer subtaskListedImage1">
+                                <img class="mRight8" src="./img/userStoryEdit/edit-dark.png" alt="edit">
+                                <img class="mRight16" src="./img/userStoryEdit/delete.png" alt="delete">
+                            </div>
+                        </div>
+                    </table>
+                </div>
+            </form>
+        </div>
+    </div>
     </div>
     `
 }
--- JS/board/userStory.js
+++ JS/board/userStory.js
@@ -23,3 +23,24 @@ function closeUserStory() {
         overlay.classList.remove("overlay");
     }, 200);
 }
+
+function openUserStoryEdit() {
+    let userStoryContainer = document.getElementById('userStoryWindow');
+    userStoryContainer.innerHTML = userStoryEditHtmlTemplate();
+}
+
+function closeUserStoryEdit() {
+    let userStoryContainer = document.getElementById('userStoryWindow');
+    let boardBodyContainer = document.querySelector('.boardBodyContainer');
+    let overlay = document.getElementById('overlay');
+
+    userStoryContainer.style.animation = 'fly-out 0.1s forwards';
+    overlay.style.animation = 'fade-out 0.2s forwards';
+    setTimeout(() => {
+        userStoryContainer.innerHTML = ``;
+        userStoryContainer.style.animation = ``;
+        boardBodyContainer.style.overflow = "";
+        overlay.style.animation = '';
+        overlay.classList.remove("overlay");
+    }, 200);
+}
