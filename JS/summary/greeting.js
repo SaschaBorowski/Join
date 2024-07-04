a312e9b9543cb3995f474a1d38fa4ba18de15587
Marcel Menke <marcel.menke1981@gmail.com>
2024-07-04 12:11:34 +0200

    fixed greeting text

--- JS/summary/greeting.js
+++ JS/summary/greeting.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let getGreeting = () => {
         let hour = new Date().getHours();
-        return hour < 12 ? "Good morning" : hour < 18 ? "Good afternoon" : "Good evening";
+        return hour < 12 ? "Good morning" : hour < 18 ? "Good evening" : "Good afternoon";
     };
 
     let greetText = document.getElementById('greetText');
