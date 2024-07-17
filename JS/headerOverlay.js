29493c0b541acd0a222d7b16537bcfe0ec43bb4d
Marcel Menke <marcel.menke1981@gmail.com>
2024-07-17 07:54:59 +0200

    added js for dropdeown menu header . extendet includeHTML.js . added headerOverlay.js

---
+++ JS/headerOverlay.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('toggleDropDown').addEventListener('click', function () {
+        let overlayHeader = document.getElementById('overlayHeader');
+        overlayHeader.classList.toggle('hidden');
+    });
+});
