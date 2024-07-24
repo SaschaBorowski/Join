352561addb1dcd256271a141a7b3e3bab87f8bf6
Mario Milaj <milajmario@gmail.com>
2024-07-24 18:39:48 +0200

    login and sign up changes

--- JS/log_in/login.js
+++ JS/log_in/login.js
@@ -1,8 +1,48 @@
-function loginGuest(){
-    let email = document.getElementById('loginEmail');
-    let pass = document.getElementById('loginPassword');
+function loginGuest() {
+  for (let i = 0; i < firebaseUsers.length; i++) {
+    const userData = firebaseUsers[i];
+    const users = userData["dataExtracted"];
+    const user = Object.keys(users);
+    for (let j = 0; j < user.length; j++) {
+      allUsers.push({
+        id: user[i],
+        dataExtracted: users[user[i++]],
+      });
+    }
+    console.log(users);
+  }
+}
 
-    email.value = 'guest@gmail.com';
-    pass.value = 'guest'
-    console.log(firebaseUsers)
+async function loadData() {
+  try {
+    let response = await fetch(BASE_URL + "users" + ".json");
+    let responseToJson = await response.json();
+
+    if (responseToJson) {
+      allData = Object.keys(responseToJson).map((key) => ({
+        id: key,
+        ...responseToJson[key],
+      }));
+    }
+  } catch (error) {
+    console.error("Error loading data:", error);
+  }
+}
+
+function logIn() {
+  let email = document.getElementById("loginEmail").value;
+  let pass = document.getElementById("loginPassword").value;
+
+  if (!email && !pass) {
+    alert("fill in your data");
+    return;
+  } else {
+    let user = allData.find((u) => u.email === email && u.password === pass);
+    if (user) {
+      console.log("User found", user);
+      window.location = "./board.html";
+    } else {
+      console.log("Invalid email or password");
+    }
+  }
 }
--- JS/log_in/signUp.js
+++ JS/log_in/signUp.js
@@ -1,15 +1,52 @@
-function signUpConfirmation() {
-    return new Promise((resolve) => {
-        let container = document.getElementById('signUpPopup');
-        let overlay = document.getElementById('overlay');
-        let body = document.getElementById('body');
-  
-        body.classList.add('hide-overflow');
-        container.classList.remove('hide');
-        overlay.classList.remove('hide');
-        
-        setTimeout(() => {
-            resolve();  // Resolve the promise after the delay
-        }, 900);
-    });
+async function addNewUser(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  let extractedData = {
+    username: getValue("signUpName"),
+    email: getValue("signUpEmail"),
+    password: getValue("signUpPassword"),
+    passwordCheck: getValue("signUpPasswordCheck"),
+  };
+
+  for (let key in extractedData) {
+    if (!extractedData[key]) {
+      alert(`${key} cannot be empty.`);
+      return;
+    }
   }
+
+  const passwordsMatch = await passwordConfiramation();
+  if (!passwordsMatch) {
+    alert("Passwords do not match. User data will not be posted.");
+    return;
+  } else {
+    await signUpConfirmation(); // Wait for the confirmation animation to complete
+    await postData("/users", extractedData);
+    window.location = "./login.html";
+  }
+}
+
+async function passwordConfiramation() {
+  let password = getValue("signUpPassword");
+  let confirmation = getValue("signUpPasswordCheck");
+  if (password !== confirmation) {
+    return false;
+  }
+  return true;
+}
+
+function signUpConfirmation() {
+  return new Promise((resolve) => {
+    let container = document.getElementById("signUpPopup");
+    let overlay = document.getElementById("overlay");
+    let body = document.getElementById("body");
+
+    body.classList.add("hide-overflow");
+    container.classList.remove("hide");
+    overlay.classList.remove("hide");
+
+    setTimeout(() => {
+      resolve(); // Resolve the promise after the delay
+    }, 900);
+  });
+}
