7e50af45f24cb512e5154ea0718b4ce57294ac19
SaschaBorowski <saschaborowski@outlook.de>
2024-08-01 02:36:22 +0200

    added responsive header menu

--- JS/mobileHeader.js
+++ JS/mobileHeader.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
     let overlayHeader = document.getElementById('mobileOverlayHeader');
-    overlayHeader.classList.toggle('hidden');
-    console.log("test");
+    overlayHeader.classList.toggle('hidden-responsive');
+    
 }
+
+
