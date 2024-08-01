b5b9b8ce99687e94fb4bc58ea28e89bcb1f729cf
SaschaBorowski <saschaborowski@outlook.de>
2024-08-01 08:05:15 +0200

    fixed scrollbar on board

--- JS/addTask/addTaskPopups.js
+++ JS/addTask/addTaskPopups.js
@@ -2,11 +2,11 @@ async function postTaskConfirmation(){
     return new Promise((resolve) => {
         let confirmation = document.getElementById("confirmation-overlay")
         confirmation.classList.remove('hide');
-
         setTimeout(() => {
-            resolve(); // Resolve the promise after the delay
-            window.location = "./board.html"
-          }, 900);
+          resolve(); // Resolve the promise after the delay
+          window.location = "./board.html"
+        }, 500);
+        
     });
 }
 
