9e830562689e6c475262aadf89296f89b626ad96
SaschaBorowski <saschaborowski@outlook.de>
2024-08-02 12:45:49 +0200

    added jsdoc to privacyPolicy_legalNotice no login js

--- JS/privacyPolicy_LegalNotice_noLogin.js
+++ JS/privacyPolicy_LegalNotice_noLogin.js
@@ -1,35 +1,60 @@
-// Funktion läuft mit einem 50ms Timeout. Kann sein das die Funktion verändert werden muss sobald die Webseite auf dem FTP ist. 
-
-
+/**
+ * Executes after the DOM has fully loaded and waits 100ms before running the functions.
+ */
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
-        let navbar = document.getElementById('sideNav');
-        let topNav = document.getElementById('topNav');
-        let navBottom = document.getElementById('nav-bottom');
-        let mobileNav = document.getElementById('mobileNav');
-        let mobileHeader = document.getElementById('headerLogoMobile');
-
-        if (navbar && topNav) { // Check if the element exists
-            navbar.style.display = "none";
-            topNav.style.display = "none"
-        }
-
-        if (navBottom) {
-            navBottom.innerHTML = `
-            <a href="/privacyPolicy_noLogin.html">Privacy Policy</a>
-            <a href="/legalNotice_noLogin.html">Legal Notice</a>
-            `;
-        }
-
-        if (mobileNav) {
-            mobileNav.style.display = "none";
-        }
-
-        if (mobileHeader) {
-            mobileHeader.style.display = "none";
-        }
-        console.log(mobileNav);
+        hideNavElements();
+        updateNavBottom();
+        hideMobileNav();
+        hideMobileHeader();
     }, 100);
-})
+});
 
+/**
+ * Hides the side navigation and top navigation elements if they exist.
+ */
+function hideNavElements() {
+    let navbar = document.getElementById('sideNav');
+    let topNav = document.getElementById('topNav');
 
+    if (navbar && topNav) {
+        navbar.style.display = "none";
+        topNav.style.display = "none";
+    }
+}
+
+/**
+ * Updates the content of the `nav-bottom` element with links to the Privacy Policy and Legal Notice.
+ */
+function updateNavBottom() {
+    let navBottom = document.getElementById('nav-bottom');
+
+    if (navBottom) {
+        navBottom.innerHTML = `
+        <a href="/privacyPolicy_noLogin.html">Privacy Policy</a>
+        <a href="/legalNotice_noLogin.html">Legal Notice</a>
+        `;
+    }
+}
+
+/**
+ * Hides the mobile navigation element if it exists.
+ */
+function hideMobileNav() {
+    let mobileNav = document.getElementById('mobileNav');
+
+    if (mobileNav) {
+        mobileNav.style.display = "none";
+    }
+}
+
+/**
+ * Hides the mobile header logo if it exists.
+ */
+function hideMobileHeader() {
+    let mobileHeader = document.getElementById('headerLogoMobile');
+
+    if (mobileHeader) {
+        mobileHeader.style.display = "none";
+    }
+}
