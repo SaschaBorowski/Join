d2ab7ba5cba0ef131ce5200dccddc7dc83c0dcd6
SaschaBorowski <saschaborowski@outlook.de>
2024-08-02 12:53:41 +0200

    added more js docs

--- JS/goBack.js
+++ JS/goBack.js
@@ -1,3 +1,10 @@
+/**
+ * Navigates the browser to the previous page in the history stack.
+ * This function uses the `history.back()` method to go back one step in the browser's history,
+ * effectively mimicking the behavior of the browser's back button.
+ *
+ * @function
+ */
 function goBack() {
     history.back();
 }
--- JS/headerOverlay.js
+++ JS/headerOverlay.js
@@ -1,8 +1,18 @@
+/**
+ * Attaches an event listener to the `DOMContentLoaded` event that sets up a click
+ * event handler for the button with the ID `toggleDropDown`. When the button is clicked,
+ * it toggles the visibility of the element with the ID `overlayHeader` by adding or removing
+ * the `hidden` class.
+ *
+ * This function ensures that the click event listener is only attached once the DOM
+ * has fully loaded, preventing potential issues with trying to access elements before
+ * they are available in the DOM.
+ *
+ * @function
+ */
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('toggleDropDown').addEventListener('click', function () {
         let overlayHeader = document.getElementById('overlayHeader');
         overlayHeader.classList.toggle('hidden');
     });
 });
-
-
